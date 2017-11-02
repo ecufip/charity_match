8acec9513fae6cabad3e72d67ad4f8a5950c2e3f
Sam Close <sclose86@gmail.com>
2017-11-02 12:47:01 +0000

    session required function working

--- application/application.py
+++ application/application.py
@@ -5,6 +5,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 render_template, flash
 from passlib.apps import custom_app_context as pwd_context
 
+from . helpers import *
+
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file
 
@@ -18,28 +20,28 @@ app.config.update(dict(
 app.config.from_envvar('APPLICATION_SETTINGS', silent=True)
 
 def connect_db():
-    """Connects to database"""
+    '''Connects to database'''
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 def get_db():
-    """
+    '''
     Opens a new database connection if there is none yet for the
     current application context
-    """
+    '''
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
+    '''Closes the database again at the end of the request.'''
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
 def init_db():
-    """Initializes the database"""
+    '''Initializes the database'''
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
@@ -47,24 +49,24 @@ def init_db():
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Command line prompt to call init_db function"""
+    '''Command line prompt to call init_db function'''
     init_db()
     print('Initialized the database.')
 
 @app.route('/')
 def index():
-    """ Accesses database and returns all charities to index template """
+    '''Accesses database and returns all charities to index template'''
     db = get_db()
     charities = db.execute('SELECT * FROM charities ORDER BY id DESC').fetchall()
     return render_template('index.html', charities=charities, user=session['email'])
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    """ Inserts new charity into database """
+    '''Inserts new charity into database'''
     if request.method == 'POST':
         
         # encrypt the password
-        hash = pwd_context.hash(request.form.get("password"))
+        hash = pwd_context.hash(request.form.get('password'))
         
         # open db connection
         db = get_db()
@@ -128,11 +130,21 @@ def login():
         # close db
         db.close()
 
-        return render_template('account.html', email = session['email'])
+        # direct to account homepage
+        return redirect(url_for('account')) 
     
     else:
         return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for('login'))
+
 @app.route('/account')
+@login_required
 def account():
-    return render_template('account.html')
+    return render_template('account.html', email = session['email'])
--- application/helpers.py
+++ application/helpers.py
@@ -1,14 +1,14 @@
-
+from flask import redirect, session, url_for
+from functools import wraps
 
 def login_required(f):
-    """
+    '''
     Decorate routes to require login.
-
     http://flask.pocoo.org/docs/0.11/patterns/viewdecorators/
-    """
+    '''
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect(url_for("login", next=request.url))
+        if session.get('email') is None:
+            return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
